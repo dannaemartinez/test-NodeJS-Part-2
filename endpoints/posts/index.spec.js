f7279f5b8eed8355437bac36d3b10ebd6d6edc55
dannaemartinez <dannae.martinez@enroutesystems.com>
2022-05-16 21:16:16 -0500

    Test Part2

---
+++ endpoints/posts/index.spec.js
@@ -0,0 +1,71 @@
+const postHandlers = require("./index");
+
+describe('Endpoints', () => {
+    describe('post', () => {
+        it('should create', async() => {
+            const mockUsers = [
+                { id: 1 },
+                { id: 2 }
+            ]
+            const post = {
+                userId: 1,
+                title: "Titulo",
+                body: "Cuerpo del post"
+            };
+
+            const req = {
+                body: post
+            };
+
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                send: jest.fn()
+            };
+
+            const axios = {
+                get: jest.fn().mockResolvedValue({ data: mockUsers }),
+                post: jest.fn().mockResolvedValue({ data: { id: 1000 } })
+            };
+
+            await postHandlers({ axios }).post(req, res);
+            expect(res.status).toHaveBeenCalledWith(201);
+            expect(axios.post).toHaveBeenCalledWith(
+                'https://jsonplaceholder.typicode.com/posts',
+                post
+            );
+            expect(res.send).toHaveBeenCalledWith({ id: 1000 });
+            expect(axios.get).toHaveBeenCalledWith(
+                'https://jsonplaceholder.typicode.com/users'
+            );
+        });
+
+        it('Should not create if the userId does not exists', async() => {
+            const mockUsers = [{ id: 1 }, { id: 2 }];
+            const post = {
+                userId: 3,
+                title: "Titulo",
+                body: "Cuerpo del post"
+            };
+
+            const req = {
+                body: post
+            };
+
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                send: jest.fn(),
+                sendStatus: jest.fn()
+            };
+
+            const axios = {
+                get: jest.fn().mockResolvedValue({ data: mockUsers }),
+                post: jest.fn().mockResolvedValue({ data: { id: 1000 } })
+            };
+
+            await postHandlers({ axios }).post(req, res);
+
+            expect(axios.post).not.toHaveBeenCalled();
+            expect(res.sendStatus).toHaveBeenCalledWith(400);
+        });
+    });
+});
